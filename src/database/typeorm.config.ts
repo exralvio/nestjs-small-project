d68b83133cc56d1f691cadeb35cfd21c0fa87866
Alvio Leegia <alvio@senja.co.uk>
2024-12-21 17:52:51 +0700

    Init database

---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+});
+
+export default AppDataSource;
