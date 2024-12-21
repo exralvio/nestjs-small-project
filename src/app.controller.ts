7480f5b2e988d93cfde06c1bf02b6ff7da85b78b
Alvio Leegia <alvio@senja.co.uk>
2024-12-21 19:07:09 +0700

    Remove default

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot(): object {
+    return {
+      status: 'success',
+      message: 'API is up and running',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
